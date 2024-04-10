a50f57e0448db8c78fa9cb28a61c5180929d2dac
Дашка <Romanova080903@yandex.ru>
2024-04-10 17:43:03 +0500

    Изменила вывод данных о фильмах

--- frontend/src/pages/MyCollectionFilmsPage.jsx
+++ frontend/src/pages/MyCollectionFilmsPage.jsx
@@ -31,15 +31,15 @@ const MainPage = () => {
         <table className="modern-table">
           <tbody>
             {data && data.length > 0 && data.map((item, index) => (
-            <tr key={`${item.id_film}-${index}`}>
+              <tr key={`${item.id_film}-${index}`}>
               <td>{item.name_film}</td>
-              <td>{item.year_film}</td>
-              <td>{item.country_film}</td>
-              <td>{new Date(item.viewing_date_film).toLocaleDateString()}</td>
-              <td>{item.rating_film}</td>
-              <td>{item.evaluation_film}</td>
-              <td>{item.duration_film && `${item.duration_film.hours}:${item.duration_film.minutes}`}</td>
-              <td>{item.age_restriction_film}</td>
+              <td>{`Год: ${item.year_film}`}</td>
+              <td>{`Страна: ${item.country_film}`}</td>
+              <td>{`Дата просмотра: ${new Date(item.viewing_date_film).toLocaleDateString()}`}</td>
+              <td>{`Рейтинг: ${item.rating_film}`}</td>
+              <td>{`Оценка: ${item.evaluation_film}`}</td>
+              <td>{`Продолжительность фильма: ${item.duration_film.hours} часа ${item.duration_film.minutes} минут`}</td>
+              <td>{`Возрасное ограничение: ${item.age_restriction_film}`}</td>
               <td>{item.has_translation_film}</td>
               <td>{item.see_Film}</td>
               <td><img src={item.photo_film} alt={`Фото ${item.name_film}`} /></td>
